af1a86c9ca08463be2c07e0522dbe513b373d1e7
Ana Luiza Fioravante Di Sciascio <ana.fioravante@ibm.com>
2021-10-08 14:35:38 -0300

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const { response } = require('express')
 //Documentation: expressjs.comhttps://expressjs.com/en/4x/api.html
 
 const app = express()
+const port = process.env.PORT || 3000 //To use Heroku or 3000 to not crash in out machine
 
 //Define paths for Express config
 const path_public = path.join(__dirname, '../public')
@@ -101,6 +102,6 @@ app.get('*', (req, res )=> {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port' + port)
 })
